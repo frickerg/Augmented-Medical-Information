6369ef2006dbd3e3c49d76509880e93760b6dbea
Josh <32765631+drewjosh@users.noreply.github.com>
2019-10-23 09:47:33 +0200

    Initial commit
    Trying to set up working version with own SceneController

---
+++ Assets/ScanStart.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using System;
+
+public class ScanStart : MonoBehaviour
+{
+  public GameObject FitToScanOverlay;
+
+  public SceneController controller;
+
+  private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+  void Start()
+  {
+    Console.WriteLine("Hello here we start");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Get updated augmented images for this frame.
+    Session.GetTrackables<AugmentedImage>(
+        m_TempAugmentedImages, TrackableQueryFilter.Updated);
+
+    if (this.m_TempAugmentedImages.Count > 0)
+    {
+      FitToScanOverlay.SetActive(false);
+      foreach (var image in m_TempAugmentedImages)
+      {
+        Console.WriteLine("Wohoo we have a picture found!");
+        this.controller.StartPosition(image);
+        this.DeactivateSelf();
+        return;
+      }
+    }
+    else
+    {
+      FitToScanOverlay.SetActive(true);
+    }
+  }
+
+  void DeactivateSelf()
+  {
+    this.gameObject.SetActive(false);
+  }
+}
---
+++ Assets/SceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class SceneController : MonoBehaviour
+{
+  public Camera firstPersonCamera;
+
+  public TestCube testCube;
+  private Anchor anchor;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  // sets the start position of the world
+  public void StartPosition(AugmentedImage image)
+  {
+    Pose location = image.CenterPose;
+    this.anchor = image.CreateAnchor(image.CenterPose);
+    testCube.SetPosition(location);
+  }
+}
