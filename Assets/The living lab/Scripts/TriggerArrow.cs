44b75bc23d6aea4d681b0332c62034fe6aebbecb
Josh <32765631+drewjosh@users.noreply.github.com>
2019-10-29 11:24:13 +0100

    No need of controller for triggering

--- Assets/The living lab/Scripts/TriggerArrow.cs
+++ Assets/The living lab/Scripts/TriggerArrow.cs
@@ -5,19 +5,8 @@ using GoogleARCore;
 
 public class TriggerArrow : MonoBehaviour
 {
-    // holds the screen controller object
-    public GameObject sceneController;
-
     // arrow that we want to enable
     public List<GameObject> arrows;
-
-    // holds the scene controller script
-    private SceneController controller;
-
-    void Awake()
-    {
-        this.controller = this.sceneController.GetComponent<SceneController>();
-    }
 
     private void OnTriggerEnter(Collider collider)
     {
