f2064cbfa5a2fe3b3a475775405cf640a80d1be8
Josh <32765631+drewjosh@users.noreply.github.com>
2019-10-30 09:01:54 +0100

    Walls are only invisible when player is running
    and are visible in Editor

---
+++ Assets/The living lab/Scripts/WallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    // holds the invisible material
+    public Material invisibleMat;
+
+    // set all walls to invisible material
+    void Start()
+    {
+        // get all renderers in this object and its children:
+        var renders = GetComponentsInChildren<Renderer>();
+        foreach (var render in renders)
+        {
+            render.material = invisibleMat;
+        }
+    }
+}
