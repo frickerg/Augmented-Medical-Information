d9adb1795494c36500cbbcc0af7cbb530ff0a046
Josh <32765631+drewjosh@users.noreply.github.com>
2019-10-31 09:08:20 +0100

    Added comment to trigger

--- Assets/The living lab/Scripts/TriggerArrow.cs
+++ Assets/The living lab/Scripts/TriggerArrow.cs
@@ -8,6 +8,10 @@ public class TriggerArrow : MonoBehaviour
     // arrows that we want to enable
     public List<GameObject> arrows;
 
+    // Is called when another object with a collider component touches this object.
+    // CAUTION: every object has per default a collider
+    // A trigger is only called when at least one of the colliding objects has a) a rigidbody component
+    // and b) when for one of the objects triggering enabled is
     private void OnTriggerEnter(Collider collider)
     {
         if(collider.gameObject.tag == "MainCamera") 
