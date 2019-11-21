64c32fcf6484eab5189b9d268e9766985986f4ff
Joshua <716-drewj2@users.noreply.gitlab.ti.bfh.ch>
2019-11-21 16:26:13 +0100

    Added audio listener to camera, renamed renderbehindwall

--- Assets/TheLivingLab/Scripts/RenderBehindWall.cs
+++ Assets/TheLivingLab/Scripts/RenderBehindWall.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obscured : MonoBehaviour
+public class RenderBehindWall : MonoBehaviour
 {
     // Source: https://answers.unity.com/questions/316064/can-i-obscure-an-object-using-an-invisible-object.html
     // This script hides the component when behind an object with invisible material (use InvisibleMask.shader)
