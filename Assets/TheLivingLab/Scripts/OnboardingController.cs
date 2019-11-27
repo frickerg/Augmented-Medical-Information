b16cde3a5e0527de8ca9efb2f20d3bb61a8cf25e
Joshua <716-drewj2@users.noreply.gitlab.ti.bfh.ch>
2019-11-27 08:05:22 +0100

    Handle no camera permission granted

--- Assets/TheLivingLab/Scripts/OnboardingController.cs
+++ Assets/TheLivingLab/Scripts/OnboardingController.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
-#if PLATFORM_ANDROID
-using UnityEngine.Android;
-#endif
 
 /*
  * Guides the user after app start to give access to camera,
@@ -15,19 +12,27 @@ public class OnboardingController : MonoBehaviour
     // the overlay to say user to scan the picture
     public GameObject FitToScanOverlay;
 
+    // is shown at start and is removed as soon as user granted permission
+    public GameObject NoPermissionOverlay;
+
     // access to methods of AnchorController
     public AnchorController anchor;
+
+    private bool isCameraPermissionGranted = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //TODO handle camera permission by ourselves to show proper message
-        // Source: https://docs.unity3d.com/Manual/android-manifest.html
-
-        this.ShowEarphonesMessage();
-
-        // check for errors
+        // check for error in beginning
         this.QuitOnConnectionErrors();
+    }
+
+    void Update()
+    {
+        if (!this.isCameraPermissionGranted)
+        {
+            this.CheckForCameraPermission();
+        }
     }
 
     // Shows message that user should use earphones for a
@@ -63,20 +68,31 @@ public class OnboardingController : MonoBehaviour
         this.FitToScanOverlay.SetActive(false);
     }
 
-    // Catches errors and tells user to give camera permission when not granted.
+    // Catches errors
     void QuitOnConnectionErrors()
     {
-        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
-        {
-            StartCoroutine(CodelabUtils.ToastAndExit(
-                "Camera permission is needed to run this application.", 5));
-        }
-        else if (Session.Status.IsError())
+        if (Session.Status.IsError())
         {
             // This covers a variety of errors.  See reference for details
             // https://developers.google.com/ar/reference/unity/namespace/GoogleARCore
-            StartCoroutine(CodelabUtils.ToastAndExit(
-                "ARCore encountered a problem connecting. Please restart the app.", 5));
+            Debug.Log("Something is broken!");
+
+            //TODO show a message
+        }
+    }
+
+    // Check for camera permission and check overlay
+    private void CheckForCameraPermission()
+    {
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            // show this when camera permission is not granted
+            this.NoPermissionOverlay.SetActive(true);
+        } else if (Session.Status == SessionStatus.Tracking)
+        {
+            this.NoPermissionOverlay.SetActive(false);
+            this.isCameraPermissionGranted = true;
+            this.ShowEarphonesMessage();
         }
     }
 }
